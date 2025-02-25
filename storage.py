bc0c3e3b67bdff5acdf3523df7d690065b565da0
SamuelRolands <samuelrolands27@gmail.com>
2025-02-25 23:08:00 +0530

    Updating Storage Working

--- storage.py
+++ storage.py
@@ -1,13 +1,19 @@
+import os
 import pandas as pd
 
 def save_move(piece, start, end, filename="data/board.xlsx"):
     """Logs a move into an Excel sheet."""
+
+    directory = os.path.dirname(filename)
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    
     df = pd.DataFrame([[piece, start, end]], columns=["Piece", "Start", "End"])
     
     try:
         existing = pd.read_excel(filename)
         df = pd.concat([existing, df], ignore_index=True)
     except FileNotFoundError:
-        pass
-
+        pass  
+    
     df.to_excel(filename, index=False)
